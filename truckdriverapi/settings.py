3786a47a4c0d0d65085157c778da56322f94d1a0
Mohd Razzeq <rzq272@gmail.com>
2023-03-22 22:04:11 +0800

    Add truckdriverapi and DRF to settings

--- truckdriverapi/settings.py
+++ truckdriverapi/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'truckdriverapi',
+    
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
