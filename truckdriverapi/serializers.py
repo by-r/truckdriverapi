2f2070d0c998a25423e1c517505d8044691fd750
Mohd Razzeq <rzq272@gmail.com>
2023-03-24 10:16:52 +0800

    Fix city and language fields not showing correctly

--- truckdriverapi/serializers.py
+++ truckdriverapi/serializers.py
@@ -3,6 +3,10 @@ from .models import Driver, Language, City
 
 
 class DriverSerializer(serializers.ModelSerializer):
+    city = serializers.CharField(source='city.name')
+    language = serializers.CharField(source='language.name')
+    
+    
     class Meta:
         model = Driver
         fields = ['name', 'mobilenum', 'email', 'city',
