e5838d19b21a54d6f3930a6aa668c3e2e69ca83f
Mohd Razzeq <rzq272@gmail.com>
2023-03-24 11:35:46 +0800

    Add correct URL routes

--- truckdriverapi/urls.py
+++ truckdriverapi/urls.py
@@ -5,7 +5,8 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-
-    path("driver/", views.driver_list, name="driver_list"),
-    path("driver/<int:pk>", views.driver_detail, name="driver_detail"),
+    path("driver", views.DriverList.as_view(), name="driver_list"), # return driver list 
+    path("driver/", views.driver_create, name="driver_create"), # create driver
+    path("driver/<int:pk>", views.driver_detail, name="driver_detail"), # return single driver detail
+    
 ]
