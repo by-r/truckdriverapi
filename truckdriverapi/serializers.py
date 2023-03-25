96daa424a21468825c419d0f68dde00b12f3abd8
Mohd Razzeq <rzq272@gmail.com>
2023-03-25 18:17:11 +0800

    Improve serializer output

--- truckdriverapi/serializers.py
+++ truckdriverapi/serializers.py
@@ -5,10 +5,11 @@ from .models import Driver, Language, City, AssignedTruck
 class DriverSerializer(serializers.ModelSerializer):
     city = serializers.CharField(source='city.name')
     language = serializers.CharField(source='language.name')
-    assigned_truck = serializers.CharField(source='assigned_truck.number_plate')
-    
+    number_plate = serializers.CharField(source='assigned_truck.number_plate')
+    registration_number = serializers.CharField(source='assigned_truck.registration_number')
     
     class Meta:
         model = Driver
         fields = ['name', 'mobilenum', 'email', 'city',
-                  'district', 'language', 'assigned_truck']
+                  'district', 'language', 'number_plate', 'registration_number']
+        
