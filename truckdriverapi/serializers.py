6c6bb3d6c7c3b1a65b29aaf5071b8f8b35783dd5
Mohd Razzeq <rzq272@gmail.com>
2023-03-26 19:28:13 +0800

    Fix error on POST

--- truckdriverapi/serializers.py
+++ truckdriverapi/serializers.py
@@ -9,16 +9,23 @@ class AssignedTruckSerializer(serializers.ModelSerializer):
 class DriverSerializer(serializers.ModelSerializer):
     city = serializers.CharField(source='city.name')
     language = serializers.CharField(source='language.name')
-    
     assigned_truck = AssignedTruckSerializer(required=True)
-    
+
     class Meta:
         model = Driver
-        fields = ['name', 'mobilenum', 'email', 'city',
-                  'district', 'language', 'assigned_truck']
-        
+        fields = ['name', 'mobilenum', 'email', 'city', 'district', 'language', 'assigned_truck']
+
     def create(self, validated_data):
         assigned_truck_data = validated_data.pop('assigned_truck')
-        assigned_truck = AssignedTruckSerializer.create(AssignedTruckSerializer(), validated_data=assigned_truck_data)
-        driver, created = Driver.objects.update_or_create(assigned_truck=assigned_truck)
+        assigned_truck = AssignedTruck.objects.create(**assigned_truck_data)
+        language_data = validated_data.pop('language')
+        language = Language.objects.get_or_create(name=language_data['name'])[0]
+        city_data = validated_data.pop('city')
+        city = City.objects.get_or_create(name=city_data['name'])[0]
+        driver = Driver.objects.create(
+            assigned_truck=assigned_truck,
+            language=language,
+            city=city,
+            **validated_data
+        )
         return driver
