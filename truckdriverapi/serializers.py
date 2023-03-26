103c7f2923fa6e07e8111021d6d5add441dc89de
Mohd Razzeq <rzq272@gmail.com>
2023-03-26 17:43:55 +0800

    Alter for AssignedTruck model & WIP: Create func

--- truckdriverapi/serializers.py
+++ truckdriverapi/serializers.py
@@ -1,15 +1,24 @@
 from rest_framework import serializers
 from .models import Driver, Language, City, AssignedTruck
 
+class AssignedTruckSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AssignedTruck
+        fields = ['number_plate', 'registration_number']
 
 class DriverSerializer(serializers.ModelSerializer):
     city = serializers.CharField(source='city.name')
     language = serializers.CharField(source='language.name')
-    number_plate = serializers.CharField(source='assigned_truck.number_plate')
-    registration_number = serializers.CharField(source='assigned_truck.registration_number')
+    
+    assigned_truck = AssignedTruckSerializer(required=True)
     
     class Meta:
         model = Driver
         fields = ['name', 'mobilenum', 'email', 'city',
-                  'district', 'language', 'number_plate', 'registration_number']
+                  'district', 'language', 'assigned_truck']
         
+    def create(self, validated_data):
+        assigned_truck_data = validated_data.pop('assigned_truck')
+        assigned_truck = AssignedTruckSerializer.create(AssignedTruckSerializer(), validated_data=assigned_truck_data)
+        driver, created = Driver.objects.update_or_create(assigned_truck=assigned_truck)
+        return driver
