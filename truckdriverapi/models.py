62fb381f0cde06bc5c666c97b2420547c032d552
Mohd Razzeq <rzq272@gmail.com>
2023-03-26 19:27:56 +0800

    Minor Tweak on few fields

--- truckdriverapi/models.py
+++ truckdriverapi/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 
 class Driver(models.Model):
-
+    
     name = models.CharField(max_length=255)
     mobilenum = models.CharField(max_length=20)
     email = models.EmailField(max_length=254)
-    city = models.ForeignKey("City", on_delete=models.CASCADE)
+    city = models.ForeignKey("City", on_delete=models.CASCADE, blank=True, null=True)
     district = models.CharField(max_length=50, blank=True, null=True)
-    language = models.ForeignKey("Language", on_delete=models.CASCADE)
+    language = models.ForeignKey("Language", on_delete=models.CASCADE,blank=True, null=True)
     assigned_truck = models.OneToOneField("AssignedTruck", on_delete=models.CASCADE, primary_key=True)
 
     def __str__(self):
@@ -26,7 +26,6 @@ class City(models.Model):
     name = models.CharField(max_length=50)
 
     class Meta:
-        verbose_name = ("City")
         verbose_name_plural = ("Cities")
 
     def __str__(self):
