9b18e3be9a0680dc4d44cb7f9d90d546ad1da1b7
Mohd Razzeq <rzq272@gmail.com>
2023-03-26 19:27:34 +0800

    Reroute driver/ to CBV

--- truckdriverapi/urls.py
+++ truckdriverapi/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     path("driver", views.DriverList.as_view(), name="driver_list"), # return driver list 
-    path("driver/", views.driver_create, name="driver_create"), # create driver
+    path("driver/", views.DriverCreate.as_view(), name="driver_create"), # create driver
     path("driver/<int:pk>", views.driver_detail, name="driver_detail"), # return single driver detail
     
 ]
